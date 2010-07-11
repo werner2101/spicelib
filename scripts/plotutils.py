b2c8e4fccab8175df02b5f751f8439120691ebc4
Werner Hoch <werner.ho@gmx.de>
2010-07-11 19:15:44 +0200

    simplified and improved gnuplot_wrapper
    added a subplot class and use subplot commands.
    removed multiplot function.

--- scripts/plotutils.py
+++ scripts/plotutils.py
@@ -13,71 +13,58 @@ else:
     import matplotlib.pyplot
 
 
-class plot_wrapper_base(object):
+class gnuplot_subplot(object):
+    """structure for a single subplot for gnuplot"""
     def __init__(self):
-        self.mplot_cnt = -1 #-1 is flag meaning 'not in multiplot mode'
-        self.layout = 1,1
-    def _mplot_idx(self):
-        if self.mplot_cnt > 0:
-            idx = self.mplot_cnt
-        else:
-            idx = 0
-        return idx
-    def multiplot(self, rows, cols):
-        self.mplot_cnt = rows * cols
-        self.layout = rows, cols
+        self.data = []
+        self.ylabel = ''
+        self.xlabel = ''
+        self.logscale = ''
 
+class gnuplot_wrapper(object):
+    """
+    A wrapper for gnuplot that behaves like matplotlib.pyplot.
+    It is used to switch between gnuplot and matplotlib.
 
-
-
-class gnuplot_wrapper(plot_wrapper_base):
+    Note: Create a new wrapper instance for each plot.
+    """
     def __init__(self):
         self.g = Gnuplot.Gnuplot()
         self.g('set data style line')
         self.g('set terminal png')
         self.g('set key left top')
-        self.dataset = [[]]
-        self.xlabels = [None]
-        self.ylabels = [None]
-        self.logscales = ['']
-        super(gnuplot_wrapper, self).__init__()
+        self.subplots = [ gnuplot_subplot() ]
+        self.current_subplot = self.subplots[0]
+        self.layout = (1,1)
 
-    def plot(self,x,y,label=None, mplot_idx = None):
-        if self.mplot_cnt == 0:
-            print >>sys.stderr, "Error, more plots requested than the multiplot layout can hold"
-        elif self.mplot_cnt > 0:
-            self.mplot_cnt -= 1
-        self.dataset[self._mplot_idx()].append(Gnuplot.Data(x, y, title=label))
+    def subplot(self, x, y, i):
+        while len(self.subplots) < x*y:
+            self.subplots.append(gnuplot_subplot())
+        self.layout = (x,y)
+        self.current_subplot = self.subplots[i-1]
+
+    def plot(self,x,y=None,label=None):
+        if y == None:
+            x, y = range(len(x)), x
+        self.current_subplot.data.append(Gnuplot.Data(x, y, title=label))
 
     def semilogx(self,x,y,label=None):
+        self.current_subplot.logscale = 'x'
         self.plot(x,y,label)
-        self.logscales[self._mplot_idx()] = 'x'
 
     def semilogy(self,x,y,label=None):
+        self.current_subplot.logscale = 'y'
         self.plot(x,y,label)
-        self.logscales[self._mplot_idx()] = 'y'
 
     def loglog(self,x,y,label=None):
+        self.current_subplot.logscale = 'xy'
         self.plot(x,y,label)
-        self.logscales[self._mplot_idx()] = 'xy'
 
-    def savefig(self, filename, **kwargs):
-        self.g('set output "%s"' % filename)
-        if self.layout != (1,1):
-            rows, cols = self.layout
-            self.g('set multiplot layout %d, %d' % (rows, cols))
-        self.dataset.reverse()
-        self.xlabels.reverse()
-        self.ylabels.reverse()
-        self.logscales.reverse()
-        for idx, data in enumerate(self.dataset):
-            logscale = self.logscales[idx]
-            self.g('unset logscale')
-            if logscale != '':
-                self.g('set logscale %s' % logscale)
-            self.g.xlabel(self.xlabels[idx])
-            self.g.ylabel(self.ylabels[idx])
-            self.g.plot(*data)
+    def xlabel(self, label):
+        self.current_subplot.xlabel = label
+        
+    def ylabel(self, label):
+        self.current_subplot.ylabel = label
 
     def legend(self, *args, **kwargs):
         pass
@@ -85,6 +72,19 @@ class gnuplot_wrapper(plot_wrapper_base):
     def grid(self, loc=None):
         self.g('set grid')
     
+    def savefig(self, filename, **kwargs):
+        self.g('set output "%s"' % filename)
+        if len(self.subplots) > 1:
+            rows, cols = self.layout
+            self.g('set multiplot layout %d, %d' % (rows, cols))
+        for sub in self.subplots:
+            self.g('unset logscale')
+            if sub.logscale != '':
+                self.g('set logscale %s' % sub.logscale)
+            self.g.xlabel(sub.xlabel)
+            self.g.ylabel(sub.ylabel)
+            self.g.plot(*sub.data)
+
     def close(self):
         #XXX We should empty the dataset here, as shown below.
         #However, Gnuplot.plot returns before the plotting is complete,
@@ -93,35 +93,15 @@ class gnuplot_wrapper(plot_wrapper_base):
         #can be used.  So we do nothing.  This means that this object
         #cannot be reused, and it is a memory leak, but the cyclic garbage
         #collector should be able to handle that.
-        #self.dataset = [[]]
+        #self.subplots = []
         pass
-
-    def xlabel(self, label):
-        self.xlabels[self._mplot_idx()] = label
-        
-    def ylabel(self, label):
-        self.ylabels[self._mplot_idx()] = label
-
-    def multiplot(self, rows, cols):
-        self.dataset = [[] for i in range(rows * cols)]
-        self.xlabels = rows * cols * [None]
-        self.ylabels = rows * cols * [None]
-        self.logscales = rows * cols * ['']
-        super(gnuplot_wrapper, self).multiplot(rows, cols)
-
-    def subplot(self, x, y, i):
-        pass    #Not needed for gnuplot backend
 
 
 def plotter():
     if PLOTTER == 'gnuplot':
         return gnuplot_wrapper()
     else:
-        def dummy(rows, cols):
-            pass
-        pp = matplotlib.pyplot
-        pp.multiplot = dummy
-        return pp
+        return matplotlib.pyplot
 
 
 def load(fname,comments='#',delimiter=None, skiprows=0,
@@ -205,3 +185,38 @@ def load(fname,comments='#',delimiter=None, skiprows=0,
     if r==1 or c==1:
         X.shape = max(r,c),
     return X
+
+
+def test_gnuplot_wrapper():
+    """ Test function for the matplotlib gnuplot wrapper """
+    pl = gnuplot_wrapper()
+    pl.plot([1,2,3], label='test123')
+    pl.savefig('123.png')
+    pl.close()
+
+    pl = gnuplot_wrapper()
+
+    pl.subplot(2,2,1)
+    pl.plot([1,2,3,7],[3,2,4,3])
+
+    pl.subplot(2,2,2)
+    pl.semilogx([1,2,34],[0.1,2,3], label='semilogx')
+    pl.xlabel('xxx1')
+
+    pl.subplot(2,2,3)
+    pl.semilogy([1,2,34],[0.1,2,3], label='semilogy')
+    pl.semilogy([2,3,4],[0.9,5,2], label='semilogy2')
+    pl.ylabel('yyy1')
+
+    pl.subplot(2,2,4)
+    pl.loglog([1,10,100], [2, 4, 8], label='loglog')
+
+    pl.grid()
+    pl.savefig('2x2_test.png')
+    pl.close()
+
+
+if __name__ == '__main__':
+    import Gnuplot
+    test_gnuplot_wrapper()
+    
